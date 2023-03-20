7990fce11b197ccbf68c2b970b69595367853828
Riley Zilka <rileyzilka33@gmail.com>
2023-03-19 18:33:23 -0600

    data gathering methods finished

--- code/get_replays.py
+++ code/get_replays.py
@@ -6,7 +6,8 @@ API_KEY = 't5E6suPaU3DWzepC78jNVOrYERrJclOKRPLrDrA7'
 #names are gathered from the following link - higher earnings means overall better performance during the year
 #https://www.esportsearnings.com/history/2022/games/409-rocket-league
 
-NAMES = ["Caard", "Chicago", "Jstn", "Firstkiller"]
+NAMES = ["Firstkiller"]
+PLAYLIST = 'ranked-duels'
 
 def gather_replays():
     '''
@@ -19,7 +20,7 @@ def gather_replays():
         print(f'STARTED getting replays for player {name}...')
 
         #get the reponse
-        response = requests.get(f'https://ballchasing.com/api/replays?player-name={name}&minimum-rank=grand-champion-3', headers=headers)
+        response = requests.get(f'https://ballchasing.com/api/replays?player-name={name}&minimum-rank=grand-champion-3&playlist={PLAYLIST}', headers=headers)
 
         #Get the replay id, name and link from the response
         i = 0
@@ -32,24 +33,148 @@ def gather_replays():
         #write the replayIDs dict to a json file
         json_object = json.dumps(replayIDs, indent=4)
 
-        with open(f'../data/replays/replays-{name}.json', 'w') as f:
+        with open(f'../data/replay-ids/{PLAYLIST}/replays-{name}.json', 'w') as f:
             f.write(json_object)
 
         print(f'DONE getting replays for {name}')
 
-def download_replays():
-    pass
+def download_positional_data():
 
+    headers = {'Authorization': API_KEY}
+    
+    for name in NAMES:
+        print(f'STARTED downloading positional data for player {name}...')
+
+        f = open(f'../data/replay-ids/{PLAYLIST}/replays-{name}.json')
+        data = json.load(f)
+
+        for i in data:
+            print(f'STARTED replay {i}...')
+
+            fileID = data[i]['id']
+            response = requests.get(f'https://ballchasing.com/dyn/replay/{fileID}/threejs', headers=headers)
+
+            res = str(response.content)
+            length = len(res)
+            parsed = res[6:length-6]
+            parsed = '{"' + parsed[0:10] + '":' + parsed[11:] + '}'
+
+            parsed = json.loads(parsed)
+
+            json_object = json.dumps(parsed, indent=4)
+
+            with open(f'../data/positional-data/{PLAYLIST}/{name}-{i}.json', 'w') as f:
+                f.write(json_object)
+
+        print(f'FINISHED downloading positional data for player {name}')
+
+def download_timeline_data():
+    
+    headers = {'Authorization': API_KEY}
+    
+    for name in NAMES:
+        print(f'STARTED downloading positional data for player {name}...')
+
+        f = open(f'../data/replay-ids/{PLAYLIST}/replays-{name}.json')
+        data = json.load(f)
+
+        for i in data:
+            print(f"STARTED replay {i}...")
+
+            fileID = data[i]['id']
+            response = requests.get(f'https://ballchasing.com/dyn/replay/{fileID}/timeline', headers=headers)
+
+            res = str(response.content)
+            length = len(res)
+            parsed = res[2:length-3]
+
+            parsed = json.loads(parsed)
+
+            json_object = json.dumps(parsed, indent=4)
+
+            with open(f'../data/timeline-data/{PLAYLIST}/{name}-{i}.json', 'w') as f:
+                f.write(json_object)
+
+        print(f'FINISHED downloading positional data for player {name}')
+
+def download_replay_stats():
+
+    headers = {'Authorization': API_KEY}
+    
+    for name in NAMES:
+        print(f'STARTED downloading replay stats for player {name}...')
+
+        f = open(f'../data/replay-ids/{PLAYLIST}/replays-{name}.json')
+        data = json.load(f)
+
+        for i in data:
+            print(f"STARTED replay {i}...")
+
+            fileID = data[i]['id']
+            response = requests.get(f'https://ballchasing.com/api/replays/{fileID}', headers=headers)
+
+            response = response.json()
+
+            json_object = json.dumps(response, indent=4)
+
+            with open(f'../data/replay-stats/{PLAYLIST}/{name}-{i}.json', 'w') as f:
+                f.write(json_object)
+
+        print(f'FINISHED downloading replay stats for player {name}')
+
+def download_replay_files():
+
+    headers = {'Authorization': API_KEY}
+    
+    for name in NAMES:
+        print(f'STARTED downloading replay files for player {name}...')
+
+        f = open(f'../data/replay-ids/{PLAYLIST}/replays-{name}.json')
+        data = json.load(f)
+
+        for i in data:
+            print(f"STARTED replay {i}...")
+
+            fileID = data[i]['id']
+            response = requests.get(f'https://ballchasing.com/api/replays/{fileID}/file', headers=headers)
+
+            with open(f'../data/replays/{PLAYLIST}/{fileID}.replay', 'wb') as f:
+                f.write(response.content)
+
+        print(f'FINISHED downloading replay files for player {name}...')
 
 def main():
 
-    ans = input('(0) Stop program\n(1) Gather replay ids\n(2) Download replay files\nChoose an option: ')
+    ans = input('(0) Stop program\n'
+                '(1) Gather replay ids\n'
+                '(2) Download positional data\n'
+                '(3) Download timeline data\n'
+                '(4) Download replay stats\n'
+                '(5) Download replay files\n'
+                'Choose an option: ')
     while ans != "0":
         if ans == "1":
             gather_replays()
+
         elif ans == "2":
-            download_replays()
-        ans = input('(0) Stop program\n(1) Gather replay ids\n(2) Download replay files\nChoose an option: ')
+            download_positional_data()
+
+        elif ans == "3":
+            download_timeline_data()
+
+        elif ans == "4":
+            download_replay_stats()
+
+        elif ans == "5":
+            download_replay_files()
+
+        ans = input('(0) Stop program\n'
+            '(1) Gather replay ids\n'
+            '(2) Download positional data\n'
+            '(3) Download timeline data\n'
+            '(4) Download replay stats\n'
+            '(5) Download replay files\n'
+            'Choose an option: ')
 
     print('Terminating...')
 
