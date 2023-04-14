d8893774e4cffc2c842c5280d7c6c3075d71fd56
Riley Zilka <rileyzilka33@gmail.com>
2023-04-13 19:09:23 -0600

    Data class finished

--- code/machine_learning/data.py
+++ code/machine_learning/data.py
@@ -1,19 +1,23 @@
 
 import numpy as np
+import json
 
 
 class Data:
 
     def __init__(self, playlist, fileCount):
         
-        self.playlist = playlist
-        self.fileCount = fileCount
+        self.__playlist = playlist
+        self.__fileCount = fileCount+1
 
         seed = np.random.randint(0, 100000)
-        self.seed = seed
+        self.__seed = seed
 
 
     def getData(self):
+        '''
+        This method will get the data for the experiment
+        '''
 
         X, y = self.loadData()
 
@@ -23,51 +27,106 @@ class Data:
 
 
     def loadData(self):
+        '''
+        This method will load all the data from the different files and get the features and targets
+        '''
 
-        data = None
-        i = None
+        data = []
 
-        chunk = self.getChunk(i)
+        for i in range(self.__fileCount):
 
-        data = self.concatenateDate(data, chunk)
+            chunk = self.getChunk(i)
 
-        X, y = self.getFeatures(data)
+            chunk = self.removeTimes(chunk)
+
+            data.extend(chunk)
+
+            X, y = self.getFeatures(data)
 
         return X, y
 
 
     def getChunk(self, i):
-
-
+        '''
+        This method will get a 'chunk' of data from the file i
+        '''
         data = None
 
-        directory = f"../../data/extracted-positional-data/{self.playlist}/{i}.json"
+        directory = f"../../data/extracted-positional-data/{self.__playlist}/{i}.json"
+
+        f = open(directory)
+        data = json.load(f)
 
         return data
 
 
-
-    def concatenateData(self, j1, j2):
+    def removeTimes(self, chunk):
         '''
-        This method will take in two jsons and splice them together to make one larger one
+        This method will remove the times from the data and add indices instead
         '''
-        j = None
+        data = []
 
+        for i in chunk:
+            data.append(chunk[i])
 
-        return j
+        return data
 
 
     def getFeatures(self, data):
+        '''
+        This method will take in the data dict and get the features X and targets y and place them into respective axis sections
+        '''
+        X, y = [], []
 
-        X, y = None
+        axisfX = []
+        axisfY = []
+        axisfZ = []
+        axistX = []
+        axistY = []
+        axistZ = []
+
+        if self.__playlist == 'ranked-duels':
+            for i in data:
+                axisfX.append([i["ball_x"], i["player_0_x"]])
+                axisfY.append([i["ball_y"], i["player_0_y"]])
+                axisfZ.append([i["ball_z"], i["player_0_z"]])
+                axistX.append(i["player_1_x"])
+                axistY.append(i["player_1_y"])
+                axistZ.append(i["player_1_z"])
+
+        axisfX = self.shuffleData(axisfX)
+        axisfY = self.shuffleData(axisfY)
+        axisfZ = self.shuffleData(axisfZ)
+        axistX = self.shuffleData(axistX)
+        axistY = self.shuffleData(axistY)
+        axistZ = self.shuffleData(axistZ)
+
+        X.append(axisfX)
+        X.append(axisfY)
+        X.append(axisfZ)
+        y.append(axistX)
+        y.append(axistY)
+        y.append(axistZ)
 
         return X, y
 
 
     def segmentData(self, X, y):
+        '''
+        This method will take in all the X data and all the target y data and segment it into the training and test sets
+        '''
 
         X_train, y_train, X_test, y_test = None, None, None, None
 
+
+        testSize = len(X[0])//10
+
+        end = len(X[0]) - testSize
+
+        X_train = [X[0][:end], X[1][:end], X[2][:end]]
+        X_test = [X[0][end:], X[1][end:], X[2][end:]]
+        y_train = [y[0][:end], y[1][:end], y[2][:end]]
+        y_test = [y[0][end:], y[1][end:], y[2][end:]]
 
         return X_train, y_train, X_test, y_test 
 
@@ -76,19 +135,25 @@ class Data:
         '''
         This method will shuffle the input array with the given seed
         '''
-        np.random.seed(self.seed)
+        np.random.seed(self.__seed)
         np.random.shuffle(array)
 
         return array
     
 
     def newSeed(self):
+        '''
+        This method will generate a new seed
+        '''
         seed = np.random.randint(0, 100000)
-        self.seed = seed
+        self.__seed = seed
 
 
     def changePlaylist(self, new):
-        self.playlist = new
+        '''
+        This method will change the playlist to gather data for it
+        '''
+        self.__playlist = new
 
     
 
--- code/machine_learning/linear_regression.py
+++ code/machine_learning/linear_regression.py
@@ -1,9 +1,16 @@
 
 import numpy as np
+import data
+
+PLAYLIST = 'ranked-duels'
+FILECOUNT = 40
 
 
 def main():
-    pass
+    d = data.Data(PLAYLIST, FILECOUNT)
+    X_train, y_train, X_test, y_test = d.getData()
+    print(len(X_train[0]))
+    print(len(X_test[0]))
 
 
 if __name__ == "__main__":
